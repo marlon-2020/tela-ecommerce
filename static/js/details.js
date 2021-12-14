fc59e131bbe711ba2470ce9f35749170d538bb97
Marlon Silva Ferreira <71356608+marlon-2020@users.noreply.github.com>
2021-12-14 12:27:47 -0300

    adiciona position fixed para o detalhamento do produto

--- static/js/details.js
+++ static/js/details.js
@@ -1,28 +1,30 @@
 var product_details = document.getElementsByClassName("product-details")
 var product_click_icon = document.getElementsByClassName("product-click-icon")
 var cart_product_grid_colunm = document.getElementsByClassName("cart-product-grid-colunm")
-var details = [{description: "Exemplo1"},{description: "Exemplo2"},{description: "Exemplo3"},
-            {description: "Exemplo4"},{description: "Exemplo5"},{description: "Exemplo6"},
-            {description: "Exemplo7"},{description: "Exemplo8"},{description: "Exemplo9"}]
+var details = [{ description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat." }, { description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat." }, { description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat." },
+{ description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat." }, { description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat." }, { description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat." },
+{ description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat." }, { description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat." }, { description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat." }]
 
-function create_div(i){
+function create_div(i) {
     var new_div = document.createElement("div")
-    new_div.className="product-container"
+    new_div.className = "product-container"
     var text_node = document.createTextNode(details[i].description)
     new_div.appendChild(text_node)
-    cart_product_grid_colunm[0].append(new_div)    
+    cart_product_grid_colunm[0].append(new_div)
+    new_div.style.position = "fixed"
+    new_div.style.top = "30%"
 }
 
-function remove_div(){
+function remove_div() {
     let product_container = document.getElementsByClassName("product-container")
     cart_product_grid_colunm[0].removeChild(product_container[9])
 }
 
 for (let i = 0; i < product_click_icon.length; i++) {
     product_click_icon[i].addEventListener("mouseover", () => {
-        create_div(i);    
+        create_div(i);
     })
-    product_click_icon[i].addEventListener("mouseout", () => {  
+    product_click_icon[i].addEventListener("mouseout", () => {
         remove_div();
     })
 }
